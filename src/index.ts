3c59496a5bbf222aec962e7b480546c2d2ee0520
Gonzalo García <gonzalo@neoland.es>
2020-11-16 12:48:24 +0100

    * src main folder of a node express with typescript project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+// Inicializamos express desde una constante llamada app, que será la que tenga toda la funcionalidad que me aporta express como framework de NODE
+const app = express();
+
+// Configurando el servidor, mediante los métodos que me da express.
+app.set('port', 3000);
+
+// levantar el servidor en el puerto configurado
+app.listen(app.get('port'), () => {
+    console.log(`El servidor se ha levantado correctamente en el puerto ${app.get('port')}`);
+});
